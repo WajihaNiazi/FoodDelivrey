840f046780f0d397963e23149d932d003dad8ff8
WajihaNiazi <wajihaniazi204@gmail.com>
2021-03-21 22:39:03 +0430

    app

--- App.js
+++ App.js
@@ -57,11 +57,4 @@ function HomeStack(){
     </Stack.Navigator>
   )
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
